99ea783b06549d89aababebef4aaea5433c3c756
dibashbikramthapa <dwasthapa172@gmail.com>
2023-04-08 19:57:57 +0545

    Login/Register with vue for single page only

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to masonite and vue authentication system.</h1>
+  </main>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,74 @@
+<script>
+
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        formClass: 'form_class',
+        user:{
+          name: '',
+          email: '',
+          password: '',
+          password_confirmation: '',
+        },
+        userDatas: ''
+      }
+      },
+      methods: {
+        increment(){
+          this.count++;
+        },
+        emailhandler(e) {
+          this.user.email = e.target.value
+      },
+      postData(e){
+        e.preventDefault()
+        debugger
+        axios.post('http://127.0.0.1:8000/create',this.user,{
+          headers : {
+              'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+          }
+      }).then(result =>
+        console.log(result))
+        
+      }
+      }
+  }
+</script>
+
+<template>
+  <!-- <h2>{{ count }}</h2>
+  <h1 :class="titleClass">{{ message }}</h1>
+  <button @click="increment">Increase count</button> -->
+  <form :class="formClass" method="POST" @submit="postData">
+    <label>Name: </label>
+    <input v-model="user.name" placeholder="Name">
+    <label>Email: </label>
+    <input @input="emailhandler" placeholder="example@gmail.com" name="email" :value="user.email" type="email" required="required">
+    <label>Password: </label>
+    <input v-model="user.password" placeholder="Type here" type="password">
+    <label>Password Confirmation: </label>
+    <input v-model="user.password_confirmation" placeholder="Type here" type="password">
+    <button type="submit">Submit</button>
+  </form>
+  <a href="/login" class="login_btn">Already registerd? Login</a>
+</template>
+
+
+<style>
+.title {
+  color: red;
+}
+.form_class{
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.3rem;
+}
+
+.login_btn{
+  margin: 1rem;
+  background-color: lightblue;
+}
+
+</style>
